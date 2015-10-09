f2d18742d24a63e78f71ba471811a5cd44fa8f00
latinman <erivas78@gmail.com>
2015-10-09 16:59:20 -0400

    Added username, username sign up & user profile pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "pages#home"
   get "/index" => "pages#index"
-  get "/profile" => "pages#profile"
+  get "/user/:id" => "pages#profile"
   get "/explore" => "pages#explore"
 
 end
